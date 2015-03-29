fa73e880c133108de2c084de689ddcfcfd3440ab
chenjs <chenjs.5492@gmail.com>
2015-03-29 22:51:47 +0800

    Completion of Lecture 5.

---
+++ Happiness/HappinessViewController.swift
@@ -0,0 +1,14 @@
+//
+//  HappinessViewController.swift
+//  Happiness
+//
+//  Created by chenjs on 15/3/29.
+//  Copyright (c) 2015年 TOMMY. All rights reserved.
+//
+
+import UIKit
+
+class HappinessViewController: UIViewController
+{
+
+}
